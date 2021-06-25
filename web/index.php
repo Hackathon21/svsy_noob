e3eedfbf853ba16eeec8f0f44cde1ddc104439f6
VanshGuptaAman <vanshgupta1608@gmail.com>
2021-06-25 18:07:44 +0530

    web start

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<!doctype HTMl>
+<html lang="en">
+    <head>
+        <title>Medix | Let's Cure Online</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- /Required meta tags -->
+
+
+        <!-- Bootstrap File Links -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <!-- /Bootstrap File Links -->
+
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 header">
+                    <div class="row">
+                        <div class="col-sm-3 logo">
+                            
+                        </div>
+                        <div class="col-sm-9 menu">
+                            
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
